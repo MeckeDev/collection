5f0461ab076ad7cb24013e23483fff3a9985b71f
MeckeDev <crafted.mecke@gmail.com>
2024-11-12 14:10:24 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: '/collection',  // This should match the name of your GitHub repository
+    }
+  }
 };
-
-export default config;
